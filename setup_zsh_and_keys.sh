add1b189224894b528cb645df32642310be235b5
Taiyo Nakashima <taiyodayo@gmail.com>
2022-04-08 13:56:19 +0900

    zsh setup script

--- setup_zsh_and_keys.sh
+++ setup_zsh_and_keys.sh
@@ -10,5 +10,5 @@ curl -ss https://github.com/taiyodayo.keys | grep ed25519 >> ~/.ssh/authorized_k
 
 # echo path for chsh -s
 echo "taiyodayo's default env and keys copied to your home directory"
-echo "(if you are not taiyo please be adviced that you have just given me access!)"
-echo "use zsh as default shell:  chsh -s $(which zsh)"
+echo "(if you are not taiyo please be adviced that you have just given me access! edit ${HOME}/.ssh/authorized_keys to revoke access)"
+echo "Use zsh as default shell:  chsh -s $(which zsh)"
