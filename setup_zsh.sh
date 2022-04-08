c66fc4bf6e03da3ccc575f1964ac97e9c95eea70
Taiyo Nakashima <taiyodayo@gmail.com>
2022-04-08 13:35:41 +0900

    タイポ修正

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 cp ~/.zshrc ~/.zshrc.bak
 cp .zshrc ~/
-cp .p10k ~/
+cp ~/.p10k.zsh ~/.p10k.zsh.bak
+cp .p10k.zsh ~/
 
 # echo path for chsh -s
 echo "use zsh as default shell:  chsh -s $(which zsh)"
