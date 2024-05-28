391ac45083823f43f4d39e1d49a4a7929540465c
taiyodayo <taiyodayo@gmail.com>
2024-05-28 13:59:07 +0900

    rbenv, cocoapods を追加

--- setup_misc.sh
+++ setup_misc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # setup brew for macs
-if [ "$(uname)" = "Darwin" ] && [ ! -f /usr/local/bin/brew ] ; then
+if [ "$(uname)" = "Darwin" ] && [ ! -f /usr/local/bin/brew ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
@@ -11,27 +11,33 @@ brew tap homebrew/cask-versions
 cat brew_list.txt | xargs brew install
 cat brew_cask.txt | xargs brew install --cask
 
-
 # setup sdkman for java
 echo "setup sdkman for java:"
 curl -s "https://get.sdkman.io" | bash && source "${HOME}/.sdkman/bin/sdkman-init.sh" && sdk install java
 
 # miniconda
 echo "Setting up miniconda"
-if [ "$(uname)" = "Linux" ] ; then
+if [ "$(uname)" = "Linux" ]; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh
 fi
 
-if [ "$(uname)" = "Darwin" ] && [ "$(uname -p)" = "arm64" ] ; then
+if [ "$(uname)" = "Darwin" ] && [ "$(uname -p)" = "arm64" ]; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
     bash Miniconda3-latest-MacOSX-arm64.sh
 fi
 
-if [ "$(uname)" = "Darwin" ] && [ "$(uname -p)" = "i386" ] ; then
+if [ "$(uname)" = "Darwin" ] && [ "$(uname -p)" = "i386" ]; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
     bash Miniconda3-latest-MacOSX-x86_64.sh
 fi
+
+# Flutter で cocoapods が必要 ruby は rbenv 使う！ rvm はトラブルだらけ
+brwe install rbenv
+rbenv init
+rbenv install 3.3.1
+rbenv global 3.3.1
+sudo gem install cocoapods
 
 # メモを表示
 echo "XcodeはAppStore経由だと不安定な事が多いです。Apple Developerから直接ダウンロードを推奨します"
@@ -44,4 +50,4 @@ echo ""
 
 echo "Flutter は brew で入れると階層が深くなるので、手動でunzipしてインストールを"
 echo "https://docs.flutter.dev/get-started/install/macos"
-echo 'export PATH="$PATH:${HOME}/flutter/bin' >> ~/.zshrc
+echo 'export PATH="$PATH:${HOME}/flutter/bin' >>~/.zshrc
