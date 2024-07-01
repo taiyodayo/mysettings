455665e5115a68185b71172c7edfc6221ec1eede
taiyo@7 default <taiyodayo@gmail.com>
2024-07-01 07:21:23 +0000

    ubuntu24に対応

--- setup_an_ubuntu_box/my_ubuntu_setup.sh
+++ setup_an_ubuntu_box/my_ubuntu_setup.sh
@@ -47,9 +47,20 @@ sudo systemctl restart docker
 
 # R の部
 # ubuntu に tidyverse で必要なパッケージ
+# キーを追加
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+# update indices
+sudo apt update -qq
+# install two helper packages we need
+sudo apt install --no-install-recommends software-properties-common dirmngr
+# add the signing key (by Michael Rutter) for these repos
+# To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
+# Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
 # cran の apt レポを追加
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo gpg --dearmor -o /usr/share/keyrings/r-project.gpg
-echo "deb [signed-by=/usr/share/keyrings/r-project.gpg] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" | sudo tee -a /etc/apt/sources.list.d/r-project.list
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+# インストール
 sudo apt update
 sudo apt install -y r-base
 # tidyverseのビルドに必要なパッケージを追加
