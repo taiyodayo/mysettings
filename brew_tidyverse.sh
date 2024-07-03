5bf9f9c9f34404126519a6e892b4311c2d97d340
taiyo@maitalk2.local default <taiyodayo@gmail.com>
2024-07-03 19:14:28 +0900

    Rのレポを山形大学に設定

--- brew_tidyverse.sh
+++ brew_tidyverse.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/env bash
+
 # brew packages required for R/tidyverse on Mac
 brew install libgit2 libsodium libtiff cmake
+brew install R
+
+# デフォルトのレポを設定
+echo 'options(repos = c(CRAN = "https://ftp.yz.yamagata-u.ac.jp/pub/cran/"))' > ~/.Rprofile
+
+# 多用するパッケージはsudo で全ユーザ向けにインストールしておく
+Rscript -e 'install.packages("pacman")'
+Rscript -e 'pacman::p_load(tidyverse, lubridate, stringr, languageserver, httpgd)'
