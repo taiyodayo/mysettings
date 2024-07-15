9c0c8c6d45168ff6d98144c78a77cd500d873691
taiyodayo <taiyodayo@users.noreply.github.com>
2024-07-15 14:02:54 +0900

    Update setup_gui_tools.sh
    added google-chrome

--- setup_ubuntu/setup_gui_tools.sh
+++ setup_ubuntu/setup_gui_tools.sh
@@ -19,3 +19,9 @@ brew install fvm
 ##
 fvm install stable
 fvm global stable
+
+# Google Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmour -o /usr/share/keyrings/google-chrome-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt update
+sudo apt install google-chrome-stable
