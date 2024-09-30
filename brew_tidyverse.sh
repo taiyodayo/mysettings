cf00ae7be5cf6518f2471daa2b170be5a9ccdf21
taiyodayo <taiyodayo@users.noreply.github.com>
2024-09-30 09:28:26 +0900

    Update brew_tidyverse.sh
    CRAN版のRを使う

--- brew_tidyverse.sh
+++ brew_tidyverse.sh
@@ -2,7 +2,10 @@
 
 # brew packages required for R/tidyverse on Mac
 brew install libgit2 libsodium libtiff cmake libxml2 openssl curl harfbuzz fribidi
-brew install R
+# こうすると CRAN ディストリビューション外の R が入る。全てのパッケージのコンパイルが必要になってしまう
+#brew install R
+# CRAN ディストリビューションはこう。これだけでパッケージのビルドが不要になる！！！
+brew install --cask r
 
 # デフォルトのレポを設定
 echo 'options(repos = c(CRAN = "https://ftp.yz.yamagata-u.ac.jp/pub/cran/"))' > ~/.Rprofile
