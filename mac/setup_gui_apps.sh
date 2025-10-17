b3273951906b2c18097daa816b5021867f20be01
taiyodayo <taiyodayo@users.noreply.github.com>
2025-10-17 19:31:18 +0900

    Dock にアプリを追加

--- mac/setup_gui_apps.sh
+++ mac/setup_gui_apps.sh
@@ -7,7 +7,13 @@ if brew list --cask | grep -q "^iterm2\$"; then
     cp "./mac/resources/com.googlecode.iterm2.plist" ~/Library/Preferences/
 fi
 
-# Mac の Finder環境をカスタム
+# dockutil を使って Dock をカスタマイズ
+brew install dockutil
+
+# メニューバーを常に隠す
+defaults write NSGlobalDomain _HIHideMenuBar -bool true && killall Finder
+
+# Mac の Dock を自動的に隠す
 defaults write com.apple.dock autohide -bool true
 # Enable magnification
 defaults write com.apple.dock magnification -bool true
@@ -18,5 +24,18 @@ defaults write com.apple.dock tilesize -int 48
 # Apply changes
 killall Dock
 
-# メニューバーを常に隠す
-defaults write NSGlobalDomain _HIHideMenuBar -bool true && killall Finder
+# アプリを Dock に追加
+# iTerm2
+dockutil --remove "/Applications/iTerm.app" --no-restart
+dockutil --add "/Applications/iTerm.app" --position end --no-restart
+# Google Chrome
+dockutil --remove "/Applications/Google Chrome.app" --no-restart
+dockutil --add "/Applications/Google Chrome.app" --position end --no-restart
+# Visual Studio Code
+dockutil --remove "/Applications/Visual Studio Code.app" --no-restart
+dockutil --add "/Applications/Visual Studio Code.app" --position end --no-restart
+# Xcode
+dockutil --remove "/Applications/Xcode.app" --no-restart
+dockutil --add "/Applications/Xcode.app" --position end --no-restart
+# Apply changes
+killall Dock
